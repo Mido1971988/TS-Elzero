ba23fadb53579f043a64bee866517117b52c3660
Mido1971988 <mohammed.soliman1971988ca@gmail.com>
2022-12-03 17:08:19 +0300

    daily

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,2 @@
 "use strict";
-/*
-difference between Statically typed language and Dynamically Typed Language:
-
-[1] Statically typed language > (Rust , C , C++)
-    Variables Type are Static , once Declared you can not change it
-    Do Type Checking at Compile-Time
-    Better Performance at Run-Time because no need to check Types Dynamically (Error Detected earlier)
-
-[2] Dynamically Typed Language > (PHP, Python, JS)
-    Variable Type is Dynamic , you can change it any time
-    Type Checking at Excution-Time (Error can be detected after Excution)
-*/
-// let num = 10;
-// num = "Elzero" // Error Type 'String' is not assignable to type 'number'
-// let age = 40;
-// if(age > 30) {
-//     console.log("Good")
-// }else{
-//     // repeat is string method not number method
-//     // will detect the Error before run the Code and even if age > 30 and will not excute this line of code
-//     console.log(age.repeat(3)) 
-// }
 //# sourceMappingURL=index.js.map
