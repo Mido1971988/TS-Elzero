3c3bd602f5a30dee5510b8dff663619717867db4
Mido1971988 <mohammed.soliman1971988ca@gmail.com>
2022-12-11 19:15:03 +0300

    Daily

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,2 @@
 "use strict";
-var Game;
-(function (Game) {
-    Game[Game["Easy"] = 100] = "Easy";
-    Game[Game["Medium"] = 80] = "Medium";
-    Game[Game["Hard"] = 30] = "Hard";
-    Game[Game["Insane"] = 20] = "Insane";
-})(Game || (Game = {}));
-console.log(Game.Easy);
-console.log(Game.Medium);
-console.log(Game.Hard);
-console.log(Game.Insane);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -687,3 +687,270 @@ Data Types
 // user.username = "Osama";
 // user.age = "40";
 // user.skills.backEnd.push(100);
+
+// ----------------------------------Interface---------------------------------
+/*
+    - Interface Declaration
+    --- Serve Like Types
+    --- The Interface Describes The Shape Of An Object
+    --- It Defines The Syntax To Follow
+
+    --- Use With Object
+    --- Use With Function
+    --- Use Read Only And Optional Operator
+*/
+
+// interface User {
+//     id?: number,
+//     readonly username: string,
+//     country: string
+// }
+
+// let user: User = {
+//     id: 100,
+//     username: "Elzero",
+//     country: "Egypt"
+// }
+
+// user.country = "Syria";
+
+// console.log(user);
+
+// function getData(data: User) {
+//     console.log(`Id Is ${data.id}`);
+//     console.log(`Username Is ${data.username}`);
+//     console.log(`Country Is ${data.country}`);
+// }
+
+// getData({ id: 200, username: "Osama", country: "KSA" });
+// getData({ username: "Osama", country: "KSA" }); // will give Error if id is not optional (id?: number)
+
+// -----------------------------------Interface Method And Parameters---------------
+
+
+// interface User  {
+//     id: number;
+//     username: string;
+//     country: string;
+//     sayHello() : string;
+//     sayWelcome: () => string;
+//     getDouble(num: number) : number;
+// }
+
+// let user: User = {
+//     id: 100,
+//     username: "Elzero",
+//     country: "Egypt",
+//     sayHello() {
+//         return `Hello ${this.username}`;
+//     },
+//     sayWelcome: () => {
+//         return `Welcome ${user.username}`;
+//     },
+//     getDouble(n) {
+//       return n * 2;
+//     }
+// }
+
+// console.log(user.id);
+// console.log(user.sayHello());
+// console.log(user.sayWelcome());
+// console.log(user.getDouble(100));
+
+// ---------------------------------------ReOpen The Interface And Use Cases------------------
+
+
+// // Homepage
+// interface Settings {
+//     readonly theme: boolean;
+//     font: string;
+// }
+
+// // Articles Page
+// interface Settings {
+//     sidebar: boolean;
+// }
+
+//   // Contact Page
+// interface Settings {
+// external: boolean;
+// }
+
+// let userSettings: Settings = {
+//     theme: true,
+//     font: "Open Sans",
+//     sidebar: false,
+//     external: true
+// }
+
+// -------------------------------------Extending Interfaces-------------------------
+
+
+// interface User {
+//     id: number;
+//     username: string;
+//     country: string;
+// }
+
+// interface Moderator {
+//     role: string | number;
+// }
+
+// interface Admin extends User,Moderator { // like & in type
+//     protect?: boolean;
+// }
+
+// let user: Admin = {
+//     id: 100,
+//     username: "Elzero",
+//     country: "Egypt",
+//     role: "Mod",
+//     protect: true
+// }
+
+// console.log(user.id);
+
+// ---------------------------------- Interface vs Type Aliases-----------------------
+/*
+** with Type you can not reopen Type and add properties but interface you can : 
+
+[1] Type : 
+type setting = {
+    font: string;
+}
+type setting = { // will give you Error
+    sidebar: boolean;
+} 
+
+[2] Interface :
+interface setting  {
+    font: string;
+}
+interface setting  { // No Error
+    sidebar: boolean;
+} 
+
+** with Interface you can use extends but with Type you use &
+
+[1] Type : type Admin = User & Moderator
+[2] Interface : interface Admin extends User,Moderator{}
+
+*/
+
+// let el = document.getElementById("id") as HTMLElement; // - Take A Look On HTMLElement Interface
+
+// // Homepage
+// type Settings = {
+//     readonly theme: boolean;
+//     font: string;
+//     sidebar: boolean;
+//     external: boolean;
+// }
+
+// let userSettings: Settings = {
+//     theme: true,
+//     font: "Open Sans",
+//     sidebar: false,
+//     external: true
+// }
+
+// ---------------------------------- Class Type Annotations-----------------------
+
+
+// class User {
+//     u: string;
+//     s: number;
+//     msg: () => string;
+//     constructor(username: string, salary: number) {
+//         this.u = username;
+//         this.s = salary;
+//         this.msg = function () {
+//             return `Hello ${this.u} Your Salary Is ${this.s}`;
+//         }
+//     }
+//     sayMsg() {
+//         return `Hello ${this.u} Your Salary Is ${this.s}`;
+//     }
+// }
+
+// let userOne = new User("Elzero", 6000);
+
+// console.log(userOne.u);
+// console.log(userOne.s);
+// console.log(userOne.msg());
+// console.log(userOne.sayMsg());
+
+// ---------------------------------- Data Access Modifiers & Parameters Properties-----------------------
+/*
+    Class
+    - Data Access Modifiers & Parameters Properties
+    --- Public
+    ------ All Members Of A Class In TypeScript Are Public
+    ------ All Public Members Can Be Accessed Anywhere Without Any Restrictions
+    --- Private
+    ------ Members Are Visible Only To That Class and it's subclasses And Are Not Accessible Outside The Class
+    --- Protected
+    ------ Same Like Private But Can Be Accessed Using The Deriving Class
+
+    - TypeScript Is A Layer On Top Of JavaScript
+    - It Should Remove All Annotations And Although Access Modifiers "Private For Example"
+*/
+
+// class User {
+//     // private username: string;
+//     // protected salary: number;
+//     // public readonly address: string;
+//     msg: () => string;
+//      // if you use Parameters Properties here no need to write it at the begining of class and no need to write this.username = username ...
+//     constructor(private username: string, protected salary: number,public readonly address: string) {
+//         this.msg = function () {
+//         return `Hello ${this.username} Your Salary Is ${this.salary}`;
+//         }
+//     }
+//     sayMsg() {
+//         return `Hello ${this.username} Your Salary Is ${this.salary}`;
+//     }
+// }
+
+// let userOne = new User("Elzero", 6000, "Cairo");
+
+// // console.log(userOne.username);
+// // console.log(userOne.salary);
+// console.log(userOne.msg());
+// console.log(userOne.sayMsg());
+
+// ---------------------------------- Class Get And Set Accessors-----------------------
+
+
+// class User {
+//     public get username(): string {
+//         return this._username;
+//     }
+//     public set username(value: string) {
+//         this._username = value;
+//     }
+//     msg: () => string;
+//     constructor(private _username: string, public salary: number, public readonly address: string) {
+//         this.msg = function () {
+//             return `Hello ${this._username} Your Salary Is ${this.salary}`;
+//         }
+//     }
+//     sayMsg() {
+//         return `Hello ${this._username} Your Salary Is ${this.salary}`;
+//     }
+//     // get username() : string {
+//     //   return this._username;
+//     // }
+//     // set username(value: string) {
+//     //   this._username = value;
+//     // }
+// }
+
+// let userOne = new User("Elzero", 6000, "Cairo");
+
+// console.log(userOne.username);
+// userOne.username = "Ahmed";
+// console.log(userOne.username);
+// console.log(userOne.salary);
+// console.log(userOne.msg());
+// console.log(userOne.sayMsg());
