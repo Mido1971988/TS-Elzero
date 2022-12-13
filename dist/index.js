10dc98541f38fbd572e61e5c0a06df40eec7b4a6
Mido1971988 <mohammed.soliman1971988ca@gmail.com>
2022-12-13 19:15:50 +0300

    Daily

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,21 @@
 "use strict";
+class Player {
+    constructor(id, title, level) {
+        this.id = id;
+        this.title = title;
+        this.level = level;
+        this.logIn = () => {
+            console.log(`Logged In`);
+        };
+        this.logOut = (msg) => {
+            console.log(`Logged Out , Good Bye`);
+        };
+    }
+}
+let player1 = new Player(100, "Elzero", 95);
+console.log(player1.id);
+console.log(player1.title);
+console.log(player1.level);
+player1.logIn();
+player1.logOut("Good Bye");
 //# sourceMappingURL=index.js.map
